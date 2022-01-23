e9ddea61c6d19fb68adcc0fe8181f0afcc1c39a7
ShlomoCodeDesktop <shlomomail16@gmail.com>
2022-01-23 22:23:23 +0200

    מיזוג הCSS לתוך הJS

--- pop-up notification.js
+++ pop-up notification.js
@@ -1,12 +1,16 @@
-socket.on("event:new_notification", function (data) {
-  function OpenNotfiPath() {
-    window.open(data.path, "_blank"); //Replace "_blank" with "_ self" for the alert to open on the same tab.
-  }
-  app.alert({
-    title: "[[notifications:new_notification]]",
-    message: data.bodyShort,
-    clickfn: OpenNotfiPath,
-    timeout: 3800,
-    type: "info",
-  });
+socket.on("event:new_notification", function(data) {
+    function OpenNotfiPath() {
+        window.open(data.path, "_blank"); //Replace "_blank" with "_ self" for the alert to open on the same tab.
+    }
+    app.alert({
+        title: "[[notifications:new_notification]]",
+        message: data.bodyShort,
+        clickfn: OpenNotfiPath,
+        alert_id: "new_notification",
+        timeout: 3800,
+        type: "info",
+    });
+    if (app.user.timeagoCode === "he") {
+        $(".alert-window").css({ right: "auto", left: "20px" });
+    }
 });
